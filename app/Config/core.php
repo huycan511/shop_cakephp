4aa1f03f3c00fffe013ca62ddbb8e09481170a77
huycan511 <nguyenquanghuy0511@gmail.com>
2019-08-12 13:28:49 +0700

    ignore

--- app/Config/core.php
+++ app/Config/core.php
@@ -396,4 +396,4 @@ Cache::config('_cake_model_', array(
 	'serialize' => ($engine === 'File'),
 	'duration' => $duration
 ));
-const FIELD = "https://shopcuahue.com";
+const FIELD = "https://shopcuahuy.com";
